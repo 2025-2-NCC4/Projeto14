5e5b17f5c03afe987a0872fee92f544b12b47610
brunosr9 <teanribeiro0@gmail.com>
2025-11-17 19:50:23 -0300

    app_final

--- src/Frontend/app.py
+++ src/Frontend/app.py
@@ -1,18 +1,19 @@
 import streamlit as st
 
-st.set_page_config(
-    page_title="PicMoney Dashboard",
-    page_icon="📊",
-    layout="wide"
-)
+st.set_page_config(page_title="PicStats", page_icon="📊", layout="wide")
+
+st.logo("imgs/logoPicStats.png")
+
+st.image("imgs/logoPicStats.png", width=300)
 
 st.sidebar.title("Navegação")
 st.sidebar.write("Selecione uma página:")
 # A navegação será automática, pois estamos usando a pasta pages/
-
-st.title("Bem-vindo ao Dashboard PicMoney")
-st.write("""
+st.title("Bem vindo ao Dashboard PicStats")
+st.write(
+    """
 Utilize o menu lateral para navegar entre:
-- Visão Geral (CEO)
-- Financeiro (CFO)
-""")
+Visão Geral (CEO)
+Financeiro (CFO)
+"""
+)
